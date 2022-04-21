e6b90d964d8415274712eca6923d44958d3da5f7
Nicolas <invisiman042@gmail.com>
2022-04-22 00:12:52 +0200

    add sms weather

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
-def add(number: int):
-    return number
+from twilio.rest import Client
 
-def addstr(number: str) -> str:
-    return number
+account_sid = 'ACc26a38a2609649a63cbd67130f12be4c'
+auth_token = 'ff10a998a8c1cb3b0cdcc2bbb68e49af'
 
-print(type(add(2)))
-print(type(addstr()))
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.☔",
+                     from_='+14152376013',
+                     to='+33638396077'
+                 )
+
+print(message.sid)
