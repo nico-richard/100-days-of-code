c85ae37aecd6e6c5e2dac88af11c7dc093fa4439
Nicolas <invisiman042@gmail.com>
2022-05-15 19:25:20 +0200

    feat(day62): add daily files

--- day61/main.py
+++ day61/main.py
@@ -1,30 +1,51 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
 WTF_CSRF_ENABLED = False
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    Bootstrap(app)
+    app.secret_key = 'akey'
 
-class MyForm(FlaskForm):
-    name = StringField('name')
+    return app
+
+app = create_app()
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=5)])
+    submit = SubmitField(label='Log In')
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+@app.route("/boot")
+def bootstrap_html():
+    login_form = LoginForm()
+    return render_template('from_bootstrap.html', form=login_form)
 
-@app.route("/login")
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    login_form = LoginForm()
+    
+    if request.method == 'POST':
+        if login_form.validate_on_submit():
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+        
+    return render_template('login.html', form=login_form)
 
 
-@app.route('/submit', methods=['GET', 'POST'])
+@app.route('/submit')
 def submit():
-    form = MyForm(meta={'csrf': False})
-    if form.validate_on_submit():
-        return redirect('login')
-    return render_template('index.html', form=form)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
