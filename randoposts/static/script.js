5c910adeb62c5a5cbe133deeeb4cceac237594e8
Nicolas <invisiman042@gmail.com>
2022-09-14 17:26:16 +0200

    remove randopost from that folder

--- randoposts/static/script.js
+++
@@ -1,9 +0,0 @@
-function trig_db() {
-    data = document.getElementById("db_data")
-    if (data.style.display == "none") {
-        data.style.display = "";
-    } else {
-        data.style.display = "none";
-    }
-    console.log(data.hidden)
-}
